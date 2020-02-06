06adfb8ea931a3ac401d54810dc94de199bf33bf
thomhie0416 <48708193+thomhie0416@users.noreply.github.com>
2020-02-07 00:21:12 +0800

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 </head>
 <body>
 	<center><div style="font-weight: 30px; font-size: 40px;">
-		<a href="regular.<?php  ?>">Click Here!</a>
+		<a href="regular.php">Click Here!</a>
 	</div></center>
 </body>
 </html>
